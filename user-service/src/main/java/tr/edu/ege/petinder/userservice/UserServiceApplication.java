2dbd781ef37c1067e7b6c9dadd7fceedd0e35ad9
yamantarkan <tarkanyamn@gmail.com>
2022-05-08 21:33:06 +0300

    improvement: user-service: user-service application created

---
+++ user-service/src/main/java/tr/edu/ege/petinder/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package tr.edu.ege.petinder.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
