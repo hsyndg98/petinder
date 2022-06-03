74e6620b98c710f73f3db43a16465c097b8af92e
yamantarkan <tarkanyamn@gmail.com>
2022-06-03 16:05:36 +0300

    improvement: discovery-service: discovery service project is created

---
+++ discovery-service/discovery-service/src/test/java/tr/edu/ege/petinder/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.edu.ege.petinder.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
