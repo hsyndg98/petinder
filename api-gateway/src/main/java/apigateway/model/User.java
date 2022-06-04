10311ac2bd4730b6483e39a74872241262c82e03
yamantarkan <tarkanyamn@gmail.com>
2022-06-04 16:22:45 +0300

    improvement: api-gateway: api-gateway model class is created

---
+++ api-gateway/src/main/java/apigateway/model/User.java
@@ -0,0 +1,49 @@
+package apigateway.model;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @CreatedDate
+    @Builder.Default
+    private Date createdAt = new Date();
+
+    @UpdateTimestamp
+    @LastModifiedDate
+    @Builder.Default
+    private Date updatedAt = new Date();
+}
